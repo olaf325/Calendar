844fc7095b78456863084f29d5f9ff7a9d191a12
Aleksandra Sierant <116669280+olaf325@users.noreply.github.com>
2023-11-17 09:56:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,52 @@
+let listaDanych; 
+const inputFile = 'data.json'; //przypisanie pliku .json
+fetch(inputFile) //odczytywanie danych z pliku data.json
+    .then(response => response.json())
+    .then(jsonData => {
+        listaDanych = jsonData.rows.map(row => ({ //formowaie układu danych
+            title: `${row.person}`,
+            start: row.leave_start,
+            end: row.leave_end,
+            allDay: false,
+            description: `zastępstwo: ${row.replaced_by}`,
+            leave_type: row.leave_type
+        }));
+        console.log("mapa danych: ",listaDanych);
+        init();
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
+
 const rodzajZwolnieniaKolor = ["red","orange","yellow","lime","green","aqua","blue","purple","pink"];
-const rodzajZwolnienia = ["czerwony", "pomarańczowy", "zolty", "lime", "zielony", "aqua", "niebieski", "fioletowy", "rozowy"];
-const listaDanych = [
-];
+const rodzajZwolnienia = ["czerwony", "pomarańczowy", "zolty", "lime", "zielony", "aqua", "niebieski", "fioletowy", "rozowy"]; // przypisać kazdy z rodzaji zwolnien
+
 const legenda = document.querySelector('#rodzaj-zwolnienia-legenda');
-rodzajZwolnienia.forEach(element=> legenda.innerHTML += '<li>- ${element}</li>');
-function getEventsFrom2DArray(array) {
-    return array.map(row => ({
-      title: row[3],  
-      start: row[0],
-      end: row[1],
-      allDay: false,  
-      description: `Replaced by: ${row[4]}`,
-      backgroundColor: rodzajZwolnieniaKolor[event.leave_type - 1]
+rodzajZwolnienia.forEach((element, index) => { //stworzenie legendy znajdujacej sie po lewej stronie ekranu
+    legenda.innerHTML += `<li color='${rodzajZwolnieniaKolor[index]}'> ${element} </li>`;
+});
+function getEvents(array) {
+    return array.map(event => ({
+        title: event.title || "Default Title",
+        start: event.start || "Default Start",
+        end: event.end || "Default End",
+        allDay: event.allDay !== undefined ? event.allDay : false,
+        description: event.description || "Default Description",
+        backgroundColor: rodzajZwolnieniaKolor[(event.leave_type || 1) - 1] || "Default Color"
     }));
 }
-const testEvents = [
-    {
-      title: "Test Event",
-      start: "2023-11-01 12:00:00",
-      end: "2023-11-01 14:00:00",
-      allDay: false,
-      description: "Test Description",
-      backgroundColor: "red",
-    },
-];
-console.log(testEvents);
 const init = () => {
     const glownyKalendarz = document.querySelector('#glowny-kalendarz');  //pobranie glównego kontenera
-    const kalendarz = new FullCalendar.Calendar(glownyKalendarz, {
+    const kalendarz = new FullCalendar.Calendar(glownyKalendarz, { //tworzenie kalendarzu na stronie
         locale: 'pl',
         initialView: 'dayGridMonth',
-        headerToolbar: {    //ułożenie danych w nagłówku
+        headerToolbar: {
             start: 'prev today next',
             center: 'title',
-            end: 'prevYear,nextYear',
+            end: 'multiMonthYear,dayGridMonth,listWeek prevYear,nextYear',
             eventDisplay: 'display'
         },
-        events: getEventsFrom2DArray(testEvents),
-    });
-    kalendarz.render(); //wypisanie
-};
-document.addEventListener('DOMContentLoaded', init); 
-const miesiaceInit = () => {
-    const miesiace = document.querySelector('#miesiace');
-    const kalendarz = new FullCalendar.Calendar(miesiace, {
-        locale: 'pl',
-        initialView: 'multiMonthYear',
-        headerToolbar: {
-            start: 'prev',
-            center: 'title',
-            end: 'next'
-        }
+        navLinks: true,
+        events: listaDanych
     });
     kalendarz.render();
 };
-document.addEventListener('DOMContentLoaded', miesiaceInit); 
+document.addEventListener('DOMContentLoaded', init);
