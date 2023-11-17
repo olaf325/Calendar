9c7d7a2eaa8a61ce00e4dce7e1c13e6919f8cd7a
Aleksandra Sierant <116669280+olaf325@users.noreply.github.com>
2023-11-17 13:38:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,37 +2,20 @@ let listaDanych;
 const inputFile = 'data.json'; //przypisanie pliku .json
 fetch(inputFile) //odczytywanie danych z pliku data.json
     .then(response => response.json())
-    .then(jsonData => {
-        listaDanych = jsonData.rows.map(row => ({ //formowaie układu danych
+    .then(jsonData => { //formowaie układu danych
+        listaDanych = jsonData.rows.map(row => ({ 
             title: `${row.person}`,
             start: row.leave_start,
-            end: row.leave_end,
-            allDay: false,
+            allDay: true,
             description: `zastępstwo: ${row.replaced_by}`,
             leave_type: row.leave_type
         }));
-        console.log("mapa danych: ",listaDanych);
         init();
     })
     .catch(error => console.error('Error fetching JSON:', error));
 
 const rodzajZwolnieniaKolor = ["red","orange","yellow","lime","green","aqua","blue","purple","pink"];
 const rodzajZwolnienia = ["czerwony", "pomarańczowy", "zolty", "lime", "zielony", "aqua", "niebieski", "fioletowy", "rozowy"]; // przypisać kazdy z rodzaji zwolnien
-
-const legenda = document.querySelector('#rodzaj-zwolnienia-legenda');
-rodzajZwolnienia.forEach((element, index) => { //stworzenie legendy znajdujacej sie po lewej stronie ekranu
-    legenda.innerHTML += `<li color='${rodzajZwolnieniaKolor[index]}'> ${element} </li>`;
-});
-function getEvents(array) {
-    return array.map(event => ({
-        title: event.title || "Default Title",
-        start: event.start || "Default Start",
-        end: event.end || "Default End",
-        allDay: event.allDay !== undefined ? event.allDay : false,
-        description: event.description || "Default Description",
-        backgroundColor: rodzajZwolnieniaKolor[(event.leave_type || 1) - 1] || "Default Color"
-    }));
-}
 const init = () => {
     const glownyKalendarz = document.querySelector('#glowny-kalendarz');  //pobranie glównego kontenera
     const kalendarz = new FullCalendar.Calendar(glownyKalendarz, { //tworzenie kalendarzu na stronie
@@ -44,9 +27,31 @@ const init = () => {
             end: 'multiMonthYear,dayGridMonth,listWeek prevYear,nextYear',
             eventDisplay: 'display'
         },
-        navLinks: true,
-        events: listaDanych
+        eventLimit: true,
+        firstDay: 1,
+        events: listaDanych,
+        eventRender: function (info) { const leaveType = info.event.extendedProps.leave_type;
+            const leaveTypeIndex = leaveType - 1; // leave_type zaczyna sie od 1, dane w tabeli od 0
+            info.el.style.backgroundColor = rodzajZwolnieniaKolor[leaveTypeIndex];
+        },
+        dateClick: function (info) {
+            zmianaDaty(kalendarz, info.date);
+            console.log(info.date);
+        }
     });
     kalendarz.render();
+    const wyborDaty = document.querySelector('#wybor-daty');
+    wyborDaty.onclick = () => {
+        const miesiac = parseInt(document.querySelector('#wybor-daty-data').value); 
+        if (!isNaN(miesiac) && miesiac >= 1 && miesiac <= 12) {
+            zmianaDaty(kalendarz.gotoDate(new Date(new Date().getFullYear(), miesiac - 1, 1)));
+        } else {
+            window.alert("Niepoprawne dane");
+        }
+    };
+    const zmianaDaty = (calendar, newDate) => {
+        calendar.gotoDate(newDate);
+        calendar.setOption('events', listaDanych);
+    };
 };
 document.addEventListener('DOMContentLoaded', init);
